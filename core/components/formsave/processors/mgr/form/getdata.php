0c672855c0ee3efe70b8c07a1ddb7a555144ab4c
Patrick Nijkamp <patrick.nijkamp@gmail.com>
2012-09-18 16:38:50 +0200

    Version 1.1.0

--- core/components/formsave/processors/mgr/form/getdata.php
+++ core/components/formsave/processors/mgr/form/getdata.php
@@ -8,15 +8,9 @@ $formArray = $form->toArray();
 
 $output = '';
 foreach($formArray['data'] as $key => $value) {
-<<<<<<< HEAD
 	if (is_array($value)) {
 		$value = implode(', ', $value);
 	}
-=======
-        if (is_array($value)) {
-            $value = json_encode($value);
-        }
->>>>>>> bb1a4e8605e0986c1aa986de6089cfcd2c616e07
 	$output .= $formSave->getChunk('fs.datarow', array(
 		'key' => $key,
 		'value' => $value
