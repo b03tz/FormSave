606b83ca1f2ec1674a07f44b7b5e55c56640e1b6
Anton <himurovich@gmail.com>
2014-05-26 10:15:53 +0300

    Create default.inc.php
    added russian lexicon lang

---
+++ core/components/formsave/lexicon/ru/default.inc.php
@@ -0,0 +1,65 @@
+<?php
+/**
+* FormSave
+*
+* Copyright 2010-11 by Patrick Nijkamp <patrick@scherpontwikkeling.nl>
+*
+* This file is part of FormSave.
+*
+* FormSave is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* FormSave is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* FormSave; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+* Suite 330, Boston, MA 02111-1307 USA
+*
+* @package FormSave
+*/
+/**
+* FormSave Russian language file
+*
+* @package FormSave
+* @subpackage lexicon
+*/
+$_lang['formsave'] = 'FormSave';
+$_lang['formsave_desc'] = 'Просмотр сохраненных форм';
+$_lang['formsave.view_forms'] = 'Просмотр сохраненных форм';
+$_lang['formsave.choose_topic'] = 'Выберите тему';
+$_lang['formsave.all_forms'] = 'Все темы';
+$_lang['formsave.form_id'] = 'ID';
+$_lang['formsave.form_topic'] = 'Тема';
+$_lang['formsave.form_data_intro'] = 'Дата';
+$_lang['formsave.form_published'] = 'Опубликовано';
+$_lang['formsave.yes'] = 'Да';
+$_lang['formsave.no'] = 'Нет';
+$_lang['formsave.select_start_date'] = 'Укажите начальную дату';
+$_lang['formsave.select_end_date'] = 'Укажите конечную дату';
+$_lang['formsave.clear'] = 'Очистить фильтр';
+$_lang['formsave.export'] = 'Экспортировать';
+$_lang['formsave.form_data'] = 'Данные формы';
+$_lang['formsave.viewdata'] = 'Просмотр данных';
+$_lang['formsave.form'] = 'Форма: ';
+$_lang['formsave.ip'] = 'IP адрес: ';
+$_lang['formsave.date'] = 'Дата: ';
+$_lang['formsave.dateformat'] = '[[+m]]/[[+d]]/[[+y]] [[+h]]:[[+m]]';
+$_lang['formsave.published_yes'] = 'Да';
+$_lang['formsave.published_no'] = 'Нет';
+$_lang['formsave.published'] = 'Опубликовано';
+$_lang['formsave.close'] = 'Закрыть';
+$_lang['formsave.choose_format'] = 'Формат экспорта';
+$_lang['formsave.help_csv'] = 'Экспортировать данные в CSV (разделены запятыми)';
+$_lang['formsave.help_csv_semicolon'] = 'Экспортировать данные в CSV (разеделены точкой с запятой)';
+$_lang['formsave.help_xml'] = 'Экспортировать данные в XML <b>(должна быть выбрана только одна тема!)</b>';
+$_lang['formsave.help_html'] = 'Экспортировать данные в HTML файл для печати';
+$_lang['formsave.form_time'] = 'Время';
+$_lang['formsave.about'] = 'О программе / Обновить';
+$_lang['formsave.ajax_loading'] = 'Загрузка...';
+$_lang['formsave.delete'] = 'Удалить';
+$_lang['formsave.delete_confirm_msg'] = 'Вы уверены, что хотите удалить эту запись?';
+$_lang['formsave.delete_confirm_title'] = 'Удалить';
